49da56cbaa2396ba86f6117303f026fbca3c0ac6
Ryan LaBouve <imryanlabouve@gmail.com>
2018-04-24 10:46:38 -0500

    Adds assertion to warn if setupController not called

--- addon/ext/route.js
+++ addon/ext/route.js
@@ -1,6 +1,13 @@
 import Mixin from '@ember/object/mixin';
+import { computed } from '@ember/object';
+import { getOwner } from '@ember/application';
 
 export default Mixin.create({
+  // TODO: we should not have to do this
+  perfService: computed(function() {
+    return getOwner(this).lookup('service:ember-perf');
+  }),
+
   setupController() {
     this.get('perfService').routeActivated(this);
     this._super(...arguments);
